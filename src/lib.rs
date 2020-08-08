cbfc0a4276a7db8f77d172890d3c409bc56e58de
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-08-08 14:53:41 +0530

    Solve case when there are no senders
    What happens when all the senders are dropped and the reciever is
    waiting? Should it wait infinitely? It should not.
    
    First, Our current Inner struct is re-named into shared, and creating a
    new inner struct which also maintains a senders field, to count the
    number of senders, and this is updated whenever clone or drop occurs on
    Sender. Whenever a drop occurs and the sender see's that its the last
    one it also performs a notify (which was only done for send). and the
    reciever while recieving checks if senders are 0, and returns a None in
    that case as Recieve is -> Option<T> now.
    
    The vice-versa case i.e no recievers but senders are present is not that
    important as senders don't block unlike recievers and can be ignored as
    the program does not wait.
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,48 @@
 use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
 
+// Decision Decisions (as per this commit):
+// - Every operation takes the lock, and thats fine if you need a non-performant channel.
+// - Sends contented with one another i.e senders are also synced btw themselves.
+// - We don't have a bounded variant of sender, which can allow some sort of sync
+//   by using a fixed size buffer. There is no back-pressure in our implementation.
+
 // Inner is Arc, because sneder and receiver need to share the same instance of Inner
 // Sender should be clonable as its mspc, but if it is done using derive proc macro
 // then it expects T to be also clonable, thus restricting our channel to only clonable
 // T's. In our case, T is wrapped around in Arc, which is clonable (obvv, thats what it is for)
 // So, Let's implement clone ourselves manually
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        // udate senders after cloning
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
         Sender {
             // this is technically legal, but not the right way
             // imagine if self.innner also implemented clone, rust won't
             // know whether this call is for Arc or inside of Arc
             // as Arc derefs into the inner type
             // So, use Arc::clone() to specifically call the clone of the arc but not inside
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last = inner.senders == 0;
+        drop(inner);
+        // check and notify if last
+        // we can wakeup everytime but unnecessary perf
+        if was_last {
+            self.shared.available.notify_one();
         }
     }
 }
@@ -27,39 +51,40 @@ impl<T> Sender<T> {
     // In the current design, there are no waiting senders as when a lock is acquired
     // the sending will work
     pub fn send(&mut self, t: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(t);
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(t);
         // drop the lock, so that the reciever can wake up and acquere
-        drop(queue);
+        drop(inner);
         // notify reciever after send happens
         // notify_one notifies one of the thread waiting for available condvar
-        self.inner.available.notify_one();
+        self.shared.available.notify_one();
     }
 }
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
     // receive should be blocking, as when there is no element
     // the receive function should make the thread wait
-    pub fn receive(&mut self) -> T {
+    pub fn receive(&mut self) -> Option<T> {
         // loop because when ever we are woken up by wait i.e when a new element is added
         // we want to pop the value.
         // loop also helps because there are no guarantees from OS that this will be woken
         // up only when there is a element, so having loop helps us wait again if there is
         // no element added
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut inner = self.shared.inner.lock().unwrap();
         loop {
-            match queue.pop_front() {
-                Some(t) => return t,
+            match inner.queue.pop_front() {
+                Some(t) => return Some(t),
+                None if inner.senders == 0 => return None,
                 None => {
                     // wait takes a mutex, because the assumption is
                     // we can't wait while still holding the mutex,
                     // because otherwise whover woke you up can't get the mutex
                     // wait gives up the lock before waiting and it also gives
                     // back the handle acquiring it so that we don't have to
-                    queue = self.inner.available.wait(queue).unwrap();
+                    inner = self.shared.available.wait(inner).unwrap();
                 }
             }
         }
@@ -67,27 +92,66 @@ impl<T> Receiver<T> {
 }
 
 // queue is Mutex, because it has to mutually be accessed by both sender and reciever without racing
-struct Inner<T> {
-    queue: Mutex<VecDeque<T>>,
+struct Shared<T> {
+    inner: Mutex<Inner<T>>,
     // Condvar uses OS based condvar internally
     // this condvar should be outside the mutex, so that when we notify using condvar the other side
     // should be able to acqure the mutex, and they can't do that if condvar is part of the mutex.
     available: Condvar,
 }
 
+struct Inner<T> {
+    queue: VecDeque<T>,
+    senders: i32,
+}
+
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default(),
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+
+    let shared = Shared {
+        inner: Mutex::new(inner),
         available: Condvar::new(),
     };
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
 
     (
         Sender {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_pong() {
+        let (mut tx, mut rx) = channel();
+        tx.send(42);
+        assert_eq!(rx.receive(), Some(42));
+    }
+
+    #[test]
+    fn closed_tx() {
+        let (tx, mut rx) = channel::<i32>();
+        drop(tx);
+        assert_eq!(rx.receive(), None)
+    }
+
+    #[test]
+    fn closed_rx() {
+        let (mut tx, rx) = channel::<i32>();
+        drop(rx);
+        tx.send(42);
+    }
+
+    #[test]
+    fn threaded_ping_pong() {}
 }
